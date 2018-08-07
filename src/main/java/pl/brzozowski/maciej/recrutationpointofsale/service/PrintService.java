9ccb36a5bc289a18ed0002b19afa25a050355115
Maciej Brzozowski <maciej.brzozowski@upaid.pl>
2018-08-07 13:20:57 +0200

    Change PrintService and remove @Autowired from field

--- src/main/java/pl/brzozowski/maciej/recrutationpointofsale/service/PrintService.java
+++ src/main/java/pl/brzozowski/maciej/recrutationpointofsale/service/PrintService.java
@@ -1,18 +1,18 @@
 package pl.brzozowski.maciej.recrutationpointofsale.service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.brzozowski.maciej.recrutationpointofsale.commons.Product;
 import pl.brzozowski.maciej.recrutationpointofsale.repository.UserOrder;
 
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
 public class PrintService {
 
     @Autowired
+    public PrintService(UserOrder userOrder) {
+        this.userOrder = userOrder;
+    }
+
     private UserOrder userOrder;
 
     public Product[] printReceiptAndClose() {
